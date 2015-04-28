128a9f297580b39ded639ab25aeb396102f34141
Neskormny <neskromny@gmail.com>
2015-04-28 03:14:50 +0300

    Randomiser added

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var https = require( 'https' );
 
-var boobsKey = {"text":"boobbies"},
-	kittensKey = {"text":"cat"},
-	boobsTime =  30 * 60 * 1000,
-	kittensTime = 90 * 60 * 1000,
+var timeArray = [2, 5, 10, 15, 30, 60, 90, 120, 180],
+	keyData = [{"text":"boobbies"}, {"text":"cat"}],
 	options = {
 		"hostname": 'hooks.slack.com',
 		"path": '/services/T03FGUZ4D/B04D6A2TW/XbMOFCyF1BjhbdHEKYXYSiZC',
 		"method": 'POST',
 		"headers": {"Content-Type": "application/json"}
-	};
+	},
+	timer;
 
 var postKeyword = function( key ) {
 	var req = https.request(options);
@@ -17,15 +16,15 @@ var postKeyword = function( key ) {
 	req.end();
 };
 
+function getRandom(data) {
+	var newKey = data[Math.floor(data.length * Math.random())];
+	return newKey;
+};
 
-setInterval(
-	postKeyword,
-	boobsTime,
-	boobsKey
-);
+function post(key) {
+	postKeyword(key);
+	clearInterval(timer);
+	timer = setInterval(post, getRandom(timeArray) * 60 * 1000, getRandom(keyData));
+};
 
-setInterval(
-	postKeyword,
-	kittensTime,
-	kittensKey
-);
+timer = setInterval(post, getRandom(timeArray) * 60, getRandom(keyData));
